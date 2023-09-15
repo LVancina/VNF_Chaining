7ec95273ea6d6706414ae7afd888e62bfe735918
LVancina <95012459+LVancina@users.noreply.github.com>
2023-09-15 11:14:12 -0700

    First Upload

---
+++ ProjController-v3.py
@@ -0,0 +1,199 @@
+from pox.core import core
+from pox.lib.util import dpid_to_str
+import pox.openflow.libopenflow_01 as of
+from pox.lib.packet import *
+# import VNF_Controller
+
+log = core.getLogger()
+
+
+class SwitchController (object):
+    """
+  A SwitchController object is created for each switch that connects.
+  A Connection object for that switch is passed to the __init__ function.
+  """
+    def __init__ (self, connection):
+        self.connection = connection
+
+        # This binds our PacketIn event listener
+        connection.addListeners(self)
+
+        # Use this table to keep track of which ethernet address is on
+        # which switch port (keys are MACs, values are ports).
+        self.macTable = {}
+
+
+    # def resend_packet (self, packet_in, out_port):
+    #     """
+    #     Instructs the switch to resend a packet that it had sent to us.
+    #     "packet_in" is the ofp_packet_in object the switch had sent to the
+    #     controller due to a table-miss.
+    #     """
+    #     msg = of.ofp_packet_out()
+    #     msg.data = packet_in
+
+    #     # Add an action to send to the specified port
+    #     action = of.ofp_action_output(port = out_port)
+    #     msg.actions.append(action)
+
+    #     # Send message to switch
+    #     self.connection.send(msg)
+
+
+    def handle_arp (self, packet, packet_in):
+        """
+        Implement switch-like behavior.
+        """
+
+        # self.macTable[packet.src] = packet_in.in_port
+
+        msg = of.ofp_flow_mod()
+        msg.match = of.ofp_match.from_packet(packet_in)
+        msg.idle_timeout = 350
+        msg.priority = 1
+        msg.actions.append(of.ofp_action_output(port=of.OFPP_ALL))
+        self.connection.send(msg)
+
+        self.macTable[packet.src] = packet_in.in_port
+
+    def act_like_switch(self, packet, packet_in):
+        # self.macTable[packet.src] = packet_in.in_port
+        msg = of.ofp_flow_mod()
+
+        # Set fields to match received packet
+        msg.match = of.ofp_match.from_packet(packet_in)
+        msg.idle_timeout = 350
+        msg.priority = 1
+
+        # if the port associated with the destination MAC of the packet is known:
+        if packet.dst in self.macTable.keys():
+            # print("src/dst recognized")
+
+            # print("Installing new flow for src: {}, dst: {}", packet.dst, packet.src)
+            log.debug("Installing flow...")
+          # Maybe the log statement should have source/destination/port?
+
+            msg.actions.append(of.ofp_action_output(port=self.macTable[packet.dst]))
+
+        else:
+            # print("Broadcasting packet")
+            # Flood the packet out everything but the input port
+            # This part looks familiar, right?
+            # self.resend_packet(packet_in, of.OFPP_ALL)
+            msg.actions.append(of.ofp_action_output(port=of.OFPP_ALL))
+
+        self.connection.send(msg)
+
+    def direct_to_chain(self, packet, packet_in):
+        '''Direct the flow throught the correct function chain.'''
+        msg = of.ofp_flow_mod()
+        src_ip = packet.payload.srcip
+        if src_ip == "10.0.1.1":
+            msg.actions.append(of.ofp_action_output(port=1))
+        elif src_ip == "10.0.2.1":
+            msg.actions.append(of.ofp_action_output(port=2))
+        elif src_ip == "10.0.3.1":
+            # self.act_like_switch(packet, packet_in)
+            msg.actions.append(of.ofp_action_output(port=4))
+            return
+
+        msg.match = of.ofp_match.from_packet(packet_in)
+        msg.idle_timeout = 360
+        msg.priority = 1
+        # print("sending msg, idle_timeout = " + str(msg.idle_timeout))
+        # print("***IP***Installing new flow for src: {}, dst: {}", packet.dst, packet.src)
+        self.connection.send(msg)
+
+    def chain_forward(self, packet, packet_in):
+        '''Simply forward to the next link of the chain'''
+        msg = of.ofp_flow_mod()
+        msg.match = of.ofp_match.from_packet(packet_in)
+        msg.idle_timeout = 360
+        msg.priority = 1
+        src_ip = packet.payload.srcip
+        if src_ip == '10.0.3.1':
+            # print("Forwarding source IP: " + str(src_ip))
+            # print("***Received return packet from: " + str(src_ip))
+            msg.actions.append(of.ofp_action_output(port=4))
+        else:
+            msg.actions.append(of.ofp_action_output(port=1))
+        # print("sending msg, idle_timeout = " + str(msg.idle_timeout))
+        print("Doing action " + str(msg.actions))
+        # print("***IP***Installing new flow for src: {}, dst: {}", packet.dst, packet.src)
+        self.connection.send(msg)
+
+    # def returnTraffic(self, packet, packet_in):
+    #     '''Only allow traffic to return to the outside world'''
+
+    #     # self.macTable[packet.src] = packet_in.in_port
+    #     msg = of.ofp_flow_mod()
+    #     msg.actions.append(of.ofp_action_output(port=4))
+    #     msg.match = of.ofp_match.from_packet(packet_in)
+    #     msg.idle_timeout = 350
+    #     msg.priority = 1
+    #     self.connection.send(msg)
+
+    def dropPacket(self, packet_in):
+        msg = of.ofp_flow_mod()
+        msg.match = of.ofp_match.from_packet(packet_in)
+        msg.idle_timeout = 350
+        msg.priority = 1
+        msg.actions.append(of.ofp_action_output(port=of.OFPP_CONTROLLER))
+        self.connection.send(msg)
+
+    def _handle_PacketIn (self, event):
+        """
+        Handles packet in messages from the switch.
+        """
+
+        packet = event.parsed # This is the parsed packet data.
+        if not packet.parsed:
+            log.warning("Ignoring incomplete packet")
+            return
+
+        packet_in = event.ofp # The actual ofp_packet_in message.
+        self.macTable[packet.src] = packet_in.in_port
+        dpid = event.connection.dpid
+        print("\n***Packet received from: " + dpid_to_str(dpid))
+        print("Packet = " + str(packet))
+
+        # Switch name
+        swName = dpid_to_str(dpid).split('-')[-1]
+
+        if isinstance(packet.next, ipv4):
+        # if packet.find('ipv4') or packet.find('udp'):
+
+            # determine if the source of the event was s1.
+            # if so, then choose a chain.
+            print('IP found')
+            if swName == '01':
+                print("Direct to chain!")
+                self.direct_to_chain(packet, packet_in)
+            # elif swName == '05':
+            #     print("Return traffic")
+            #     self.returnTraffic(packet, packet_in)
+            else:
+                print("Chain forward!")
+                self.chain_forward(packet, packet_in)
+        else:
+            if swName == '01' or swName == '05':
+                print("handle ARP")
+                self.handle_arp(packet, packet_in)
+            else:
+                print("Dropping packet")
+                self.dropPacket(packet_in)
+
+
+def launch ():
+    """
+  Starts the component
+  """
+    def start_switch (event):
+        # event.connection.listen(port=6653)
+        log.debug("Controlling %s" % (event.connection,))
+        # print(str(event.ofp))
+        print("Connection dpid: " + str(event.connection.dpid))
+        SwitchController(event.connection)
+
+    core.openflow.addListenerByName("ConnectionUp", start_switch)
+
---
+++ proj-topo-v2.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+
+
+def myNetwork():
+
+    net = Mininet(switch=OVSSwitch)
+
+    info('*** Adding controller\n')
+    c0 = net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6633)
+
+    info('*** Add switches\n')
+    s1 = net.addSwitch('s1', cls=OVSSwitch, stp=True)
+    s2 = net.addSwitch('s2', cls=OVSSwitch, stp=True)
+    s3 = net.addSwitch('s3', cls=OVSSwitch, stp=True)
+    s4 = net.addSwitch('s4', cls=OVSSwitch, stp=True)
+    s5 = net.addSwitch('s5', cls=OVSSwitch, stp=True)
+
+    info('*** Add hosts\n')
+    h1 = net.addHost('h1', ip='10.0.1.1')
+    h2 = net.addHost('h2', ip='10.0.2.1')
+    h3 = net.addHost('h3', ip='10.0.3.1')
+
+    info('*** Add links\n')
+    net.addLink(s5, s1, port1=4, port2=4)
+    net.addLink(h3, s5, port2=1)
+    net.addLink(s5, s3, port2=1)
+    net.addLink(s5, s4, port2=1)
+    net.addLink(s2, s3, port1=1)
+    net.addLink(s1, s2, port1=1)
+    net.addLink(s1, s4, port1=2)
+    # net.addLink(s5, s1, port1=4)
+    net.addLink(h1, s1)
+    net.addLink(h2, s1)
+
+    info('*** Starting controllers\n')
+    s2.start([c0])
+
+    info('*** Starting network\n')
+    net.start()
+
+    s1.cmd('ovs-vsctl set bridge s1 stp_enable=true')
+    s2.cmd('ovs-vsctl set bridge s2 stp_enable=true')
+    s3.cmd('ovs-vsctl set bridge s3 stp_enable=true')
+    s4.cmd('ovs-vsctl set bridge s4 stp_enable=true')
+    s5.cmd('ovs-vsctl set bridge s5 stp_enable=true')
+
+    # h1.cmd('ifconfig h1-eth0 10.0.1.0')
+    # h1.cmd('route add default gw 10.0.1.0')
+    # h2.cmd('ifconfig h2-eth0 10.0.2.0')
+    # h2.cmd('route add default gw 10.0.2.0')
+    # h3.cmd('ifconfig h3-eth0 10.0.3.0')
+    # h3.cmd('route add default gw 10.0.3.0')
+
+    info('*** Post configure switches and hosts\n')
+
+    CLI(net)
+    net.stop()
+
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    myNetwork()
